lomoljo/Bern

support C++20 Modules, add C++20 related tools

scripts/bootstrap/compile.sh
@@ -281,6 +281,10 @@ EOF
   link_file "${PWD}/tools/cpp/runfiles/BUILD.tools" \
       "${BAZEL_TOOLS_REPO}/tools/cpp/runfiles/BUILD"
 
+  # Create @bazel_tools//tools/cpp/cpp20modules_tools
+  mkdir -p ${BAZEL_TOOLS_REPO}/tools/cpp/cpp20modules_tools
+  link_file "${PWD}/tools/cpp/cpp20modules_tools/BUILD.tools" "${BAZEL_TOOLS_REPO}/tools/cpp/cpp20modules_tools/BUILD"
+  
   # Create @bazel_tools//tools/sh
   mkdir -p ${BAZEL_TOOLS_REPO}/tools/sh
   link_file "${PWD}/tools/sh/sh_configure.bzl" "${BAZEL_TOOLS_REPO}/tools/sh/sh_configure.bzl"

src/main/starlark/builtins_bzl/common/cc/cc_toolchain.bzl
@@ -98,6 +98,8 @@ def _attributes(ctx):
         objcopy_files = _files(ctx, "objcopy_files"),
         link_dynamic_library_tool = ctx.file._link_dynamic_library_tool,
         grep_includes = grep_includes,
+        agg_ddi = _single_file(ctx, "_agg_ddi"),
+        gen_modmap = _single_file(ctx, "_gen_modmap"),
         module_map = ctx.attr.module_map,
         as_files = _files(ctx, "as_files"),
         ar_files = _files(ctx, "ar_files"),
@@ -341,6 +343,16 @@ The label of the rule providing <code>cc_toolchain_config_info</code>.""",
         "_build_info_translator": attr.label(
             default = semantics.BUILD_INFO_TRANLATOR_LABEL,
             providers = [OutputGroupInfo],
+        ),
+        "_agg_ddi": attr.label(
+            executable = True,
+            cfg = "exec",
+            default = "@" + semantics.get_repo() + "//tools/cpp:agg-ddi",
+        ),
+        "_gen_modmap": attr.label(
+            executable = True,
+            cfg = "exec",
+            default = "@" + semantics.get_repo() + "//tools/cpp:gen-modmap",
         ),
     },
 )

src/test/java/com/google/devtools/build/lib/packages/util/Crosstool.java
@@ -582,7 +582,18 @@ public final class Crosstool {
                 // We add an empty :malloc target in case we need it.
                 "cc_library(name = 'malloc')",
                 // Fake targets to get us through loading/analysis.
-                "exports_files(['grep-includes', 'link_dynamic_library'])");
+                "exports_files(['grep-includes', 'link_dynamic_library'])",
+                "",
+                "filegroup(",
+                "    name = 'agg-ddi',",
+                "    srcs = ['agg-ddi.sh'],",
+                ")",
+                "",
+                "filegroup(",
+                "    name = 'gen-modmap',",
+                "    srcs = ['gen-modmap.sh'],",
+                ")"
+            );
 
     config.create(crosstoolTop + "/mock_version/x86/bin/gcc");
     config.create(crosstoolTop + "/mock_version/x86/bin/ld");
@@ -598,6 +609,8 @@ public final class Crosstool {
     config.create(crosstoolTop + "/grep-includes");
     config.create(crosstoolTop + "/build_interface_so");
     config.create(crosstoolTop + "/link_dynamic_library");
+    config.create(crosstoolTop + "/agg-ddi.sh");
+    config.create(crosstoolTop + "/gen-modmap.sh");
   }
 
   public void writeOSX() throws IOException {

PikachuHyA/bazel

scripts/bootstrap/compile.sh
@@ -281,6 +281,10 @@ EOF
   link_file "${PWD}/tools/cpp/runfiles/BUILD.tools" \
       "${BAZEL_TOOLS_REPO}/tools/cpp/runfiles/BUILD"
 
+  # Create @bazel_tools//tools/cpp/modules_tools
+  mkdir -p ${BAZEL_TOOLS_REPO}/tools/cpp/modules_tools
+  link_file "${PWD}/tools/cpp/modules_tools/BUILD.tools" "${BAZEL_TOOLS_REPO}/tools/cpp/modules_tools/BUILD"
+  
   # Create @bazel_tools//tools/sh
   mkdir -p ${BAZEL_TOOLS_REPO}/tools/sh
   link_file "${PWD}/tools/sh/sh_configure.bzl" "${BAZEL_TOOLS_REPO}/tools/sh/sh_configure.bzl"

src/main/starlark/builtins_bzl/common/cc/cc_toolchain.bzl
@@ -98,6 +98,8 @@ def _attributes(ctx):
         objcopy_files = _files(ctx, "objcopy_files"),
         link_dynamic_library_tool = ctx.file._link_dynamic_library_tool,
         grep_includes = grep_includes,
+        agg_ddi = _single_file(ctx, "_agg_ddi"),
+        gen_modmap = _single_file(ctx, "_gen_modmap"),
         module_map = ctx.attr.module_map,
         as_files = _files(ctx, "as_files"),
         ar_files = _files(ctx, "ar_files"),
@@ -341,6 +343,16 @@ The label of the rule providing <code>cc_toolchain_config_info</code>.""",
         "_build_info_translator": attr.label(
             default = semantics.BUILD_INFO_TRANLATOR_LABEL,
             providers = [OutputGroupInfo],
+        ),
+        "_agg_ddi": attr.label(
+            executable = True,
+            cfg = "exec",
+            default = "@" + semantics.get_repo() + "//tools/cpp:aggregate-ddi",
+        ),
+        "_gen_modmap": attr.label(
+            executable = True,
+            cfg = "exec",
+            default = "@" + semantics.get_repo() + "//tools/cpp:generate-modmap",
         ),
     },
 )

src/test/java/com/google/devtools/build/lib/packages/util/Crosstool.java
@@ -582,7 +582,18 @@ public final class Crosstool {
                 // We add an empty :malloc target in case we need it.
                 "cc_library(name = 'malloc')",
                 // Fake targets to get us through loading/analysis.
-                "exports_files(['grep-includes', 'link_dynamic_library'])");
+                "exports_files(['grep-includes', 'link_dynamic_library'])",
+                "",
+                "filegroup(",
+                "    name = 'aggregate-ddi',",
+                "    srcs = ['aggregate-ddi.sh'],",
+                ")",
+                "",
+                "filegroup(",
+                "    name = 'generate-modmap',",
+                "    srcs = ['generate-modmap.sh'],",
+                ")"
+            );
 
     config.create(crosstoolTop + "/mock_version/x86/bin/gcc");
     config.create(crosstoolTop + "/mock_version/x86/bin/ld");
@@ -598,6 +609,8 @@ public final class Crosstool {
     config.create(crosstoolTop + "/grep-includes");
     config.create(crosstoolTop + "/build_interface_so");
     config.create(crosstoolTop + "/link_dynamic_library");
+    config.create(crosstoolTop + "/aggregate-ddi.sh");
+    config.create(crosstoolTop + "/generate-modmap.sh");
   }
 
   public void writeOSX() throws IOException {